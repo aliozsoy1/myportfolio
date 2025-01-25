3c9336168b5ad1d6a73e40dc84f510bec4ac7195
aliozsoy1 <aliozsoy237@gmail.com>
2025-01-25 22:43:09 +0300

    portfolio upload

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Ali Özsoy - Portfolio',
+  description: 'Frontend Developer Portfolio Sitesi',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="tr" className="scroll-smooth">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
